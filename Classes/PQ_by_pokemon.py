525c9234568b6115e35011b432dd619e23fb3175
Roey Feingold <roey.feingold@gmail.com>
2022-01-07 16:34:18 +0200

    update PQ_by_pokemon.py

--- Classes/PQ_by_pokemon.py
+++ Classes/PQ_by_pokemon.py
@@ -1,6 +1,5 @@
+from Classes import Pokemon
 from heapq import *
-
-from Classes.Pokemon import *
 
 
 class PQByPokemon(object):
@@ -9,15 +8,21 @@ class PQByPokemon(object):
         self.n: int = 0
 
     def add(self, poke: Pokemon):
-        self.n += 1
         heappush(self.Q, ((-1) * poke.value, self.n, poke))
+        self.n += 1
 
     def pop(self):
         return heappop(self.Q)[2]
 
     def push_pop(self, poke: Pokemon):
-        self.n += 1
         heappushpop(self.Q, ((-1) * poke.value, self.n, poke))
+        self.n += 1
+
+    def get_w(self, i: int):
+        return self.Q[i][0]
+
+    def get_id(self, i: int):
+        return self.Q[i][1]
 
     def is_empty(self):
         return len(self.Q) == 0
@@ -28,17 +33,17 @@ class PQByPokemon(object):
     def __str__(self):
         return self.Q.__str__()
 
-    def peek(self):
-        return self.Q[2]
+    def peek(self) -> tuple:
+        return self.Q[0]
 
     def peek_val(self) -> float:
-        return self.Q[2].value
+        return self.Q[0].value
 
     def peek_pos(self) -> tuple:
-        return self.Q[2].pos
+        return self.Q[0].pos
 
     def peek_src(self) -> int:
-        return self.Q[2].src
+        return self.Q[0].src
 
     def peek_dst(self) -> int:
-        return self.Q[2].dst
+        return self.Q[0].dst
