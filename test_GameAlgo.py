cd26c76bd1758f41cb27d48940558c4de1226273
Roey Feingold <roey.feingold@gmail.com>
2022-01-08 19:15:05 +0200

    Update GameAlgo.py & test

---
+++ test_GameAlgo.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+
+
+class TestGameAlgo(TestCase):
+
+    def test_init_pokemons(self):
+        self.fail()
+
+    def test_init_graph(self):
+        self.fail()
+
+    def test_init_agents(self):
+        self.fail()
+
+    def test_allocate_all_agents(self):
+        self.fail()
+
+    def test_choose_agent(self):
+        self.fail()
+
+    def test_time_to_poke(self):
+        self.fail()
+
+    def test_find_pok_src_dst(self):
+        self.fail()
+
+    def test_find_dist_nodes(self):
+        self.fail()
+
+    def test_all_pok_src_dst(self):
+        self.fail()
+
+    def test_cmd(self):
+        self.fail()
+
+    def test_begining_of_the_game(self):
+        self.fail()
+
+    def test_game_algorithm(self):
+        self.fail()
