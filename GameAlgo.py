00dbfbaedcd80631b94d0811e158e3b7ab482848
Roey Feingold <roey.feingold@gmail.com>
2022-01-08 17:05:22 +0200

    Update UML

--- GameAlgo.py
+++ GameAlgo.py
@@ -21,16 +21,16 @@ class GameAlgo:
         self.pokemons = PQByPokemon()
         self.agents = dict()
 
-    def update_game(self, pokemons = None , agents = None, graph =None):
+    def update_game(self, pokemons=None, agents=None, graph=None):
         if pokemons != None:
             self.init_pokemons(pokemons)
-        if agents !=None:
+        if agents != None:
             self.init_agents(agents)
         if graph != None:
             self.init_graph(graph)
         self.all_pok_src_dst()
 
-    def init_pokemons(self,pokemons):
+    def init_pokemons(self, pokemons):
         self.pokemons.reset()
         pokemon_obj = json.loads(pokemons)
         for p in pokemon_obj['Pokemons']:
@@ -60,11 +60,12 @@ class GameAlgo:
         choosenAgent = -1
         for a in self.agents.values():
             if a.next_pokemon == None:
-                tempDist = self.time_to_poke(a.id,currPoke.src)
+                tempDist = self.time_to_poke(a.id, currPoke.src)
                 if tempDist < minDist:
                     minDist = tempDist
                     choosenAgent = a.id
-            self.agents.get(choosenAgent).path = self.graph.shortest_path( self.agents.get(choosenAgent).src, currPoke.src)[1]
+            self.agents.get(choosenAgent).path = \
+            self.graph.shortest_path(self.agents.get(choosenAgent).src, currPoke.src)[1]
             self.agents.get(choosenAgent).path.append(currPoke.dst)
             self.agents.get(choosenAgent).path.pop(0)
             self.agents.get(choosenAgent).next_pokemon = 1
@@ -72,51 +73,44 @@ class GameAlgo:
                 return choosenAgent
         return None
 
-
-
     def time_to_poke(self, agent_id: int, poke_src: int) -> float:
         agent = self.agents.get(agent_id)
         dist = self.graph.shortest_path(agent.src, poke_src)[0]
         return dist / agent.speed
 
-
-    def find_pok_src_dst(self , pokemon :Pokemon):
+    def find_pok_src_dst(self, pokemon: Pokemon):
         # Finding on which edge the pokemon is and update the pokemon values
         for node1 in self.graph.get_graph().nodes.values():
             for node2 in self.graph.get_graph().nodes.values():
-                nodeDist = GameAlgo.find_dist_nodes(self,node1,node2)
+                nodeDist = GameAlgo.find_dist_nodes(self, node1, node2)
                 PokeDist = pokemon[2].dist_pokemon_node(node1) + pokemon[2].dist_pokemon_node(node2)
-                #if the distances is almost equals
+                # if the distances is almost equals
                 if abs(nodeDist - PokeDist) <= Epsilon:
-                    #check the type in order to decide which one is SRC and which one is DST
-                    if pokemon[2].type == 1: # DST  > SRC
-                        src = min(int(node1.id),int(node2.id))
-                        dst = max(int(node1.id),int(node2.id))
+                    # check the type in order to decide which one is SRC and which one is DST
+                    if pokemon[2].type == 1:  # DST  > SRC
+                        src = min(int(node1.id), int(node2.id))
+                        dst = max(int(node1.id), int(node2.id))
                     else:
-                        src = max(int(node1.id),int(node2.id))
-                        dst = min(int(node1.id),int(node2.id))
-                    #check if there is a edge between SRC and DST is so update pokemon values
+                        src = max(int(node1.id), int(node2.id))
+                        dst = min(int(node1.id), int(node2.id))
+                    # check if there is a edge between SRC and DST is so update pokemon values
                     if DiGraph.st_edge(src, dst) in self.graph.get_graph().edges:
                         pokemon[2].src = src
                         pokemon[2].dst = dst
                         return
 
-
-
-    def find_dist_nodes(self,node1 : Node , node2 :Node):
+    def find_dist_nodes(self, node1: Node, node2: Node):
         # Same formula to find dist between 2 nodes
-        delta_x = pow(node1.pos[0] - node2.pos[0],2)
+        delta_x = pow(node1.pos[0] - node2.pos[0], 2)
         delta_y = pow(node1.pos[1] - node2.pos[1], 2)
         return math.sqrt(delta_x + delta_y)
-
-
 
     def all_pok_src_dst(self):
         for pokemon in self.pokemons.Q:
             if pokemon[2].src is None and pokemon[2].dst is None:
-                GameAlgo.find_pok_src_dst(self,pokemon)
+                GameAlgo.find_pok_src_dst(self, pokemon)
 
-    def CMD(self,Client:Client):
+    def CMD(self, Client: Client):
         for a in self.agents.values():
             if a.dst == -1:
                 if a.path:
@@ -124,7 +118,7 @@ class GameAlgo:
                     if not a.path:
                         a.next_pokemon = None
 
-    def begining_of_the_game(self, Client:Client):
+    def begining_of_the_game(self, Client: Client):
         self.init_pokemons(Client.get_pokemons())
         self.init_graph(Client.get_graph())
         self.all_pok_src_dst()
@@ -133,11 +127,12 @@ class GameAlgo:
         for i in range(0, int(blaObj['GameServer']['agents'])):
             Client.add_agent('{\"id\":' + str(str(self.pokemons.pop().src)) + '}')
 
-    def game_algorithm(self,Client:Client):
+    def game_algorithm(self, Client: Client):
         self.allocate_all_agents()
         self.CMD(Client)
-        #if self.any_aloocated:
+        # if self.any_aloocated:
         Client.move()
+
 
 def main():
     PORT = 6666
@@ -145,26 +140,26 @@ def main():
     HOST = '127.0.0.1'
     MyClient = Client()
     MyClient.start_connection(HOST, PORT)
-    #bla = MyClient.get_info()
-    #blaObj = json.loads(bla)
+    # bla = MyClient.get_info()
+    # blaObj = json.loads(bla)
     game = GameAlgo()
-    #print("Hera re the pokemons")
-    #game.init_pokemons(MyClient.get_pokemons())
-    #game.init_graph(MyClient.get_graph())
-    #print(game.pokemons)
-    #game.all_pok_src_dst()
-    #print(len(game.agents))
-    #print(str(game.pokemons.pop().src))
-    #for i in range(0 , int(blaObj['GameServer']['agents'])):
-        #MyClient.add_agent('{\"id\":'+ str(str(game.pokemons.pop().src)) +'}')
-    #MyClient.add_agent("{\"id\":1}")
-    #MyClient.add_agent("{\"id\":2}")
-    #MyClient.add_agent("{\"id\":3}")
-    #MyClient.start()
+    # print("Hera re the pokemons")
+    # game.init_pokemons(MyClient.get_pokemons())
+    # game.init_graph(MyClient.get_graph())
+    # print(game.pokemons)
+    # game.all_pok_src_dst()
+    # print(len(game.agents))
+    # print(str(game.pokemons.pop().src))
+    # for i in range(0 , int(blaObj['GameServer']['agents'])):
+    # MyClient.add_agent('{\"id\":'+ str(str(game.pokemons.pop().src)) +'}')
+    # MyClient.add_agent("{\"id\":1}")
+    # MyClient.add_agent("{\"id\":2}")
+    # MyClient.add_agent("{\"id\":3}")
+    # MyClient.start()
 
-    #game = GameAlgo()
-    #game.init_pokemons(MyClient.get_pokemons())
-    #game.init_graph(MyClient.get_graph())
+    # game = GameAlgo()
+    # game.init_pokemons(MyClient.get_pokemons())
+    # game.init_graph(MyClient.get_graph())
     game.begining_of_the_game(MyClient)
     game.update_game(MyClient.get_pokemons(), MyClient.get_agents(), MyClient.get_graph())
     gui = GameGUI.GameGui(game)
@@ -177,17 +172,18 @@ def main():
         print(MyClient.time_to_end())
         print(MyClient.get_info())
         time.sleep(0.05)
-    #game.all_pok_src_dst()
-    #game.init_agents(MyClient.get_agents())
-    #print(game.pokemons)
-    #print(game.time_to_poke(0, 9))
-    #print(game.time_to_poke(1, 9))
-    #print(game.time_to_poke(2, 9))
-    #temp = game.choose_agent()
-    #print(temp)
-    #print(game.agents.get(temp).path)
+    # game.all_pok_src_dst()
+    # game.init_agents(MyClient.get_agents())
+    # print(game.pokemons)
+    # print(game.time_to_poke(0, 9))
+    # print(game.time_to_poke(1, 9))
+    # print(game.time_to_poke(2, 9))
+    # temp = game.choose_agent()
+    # print(temp)
+    # print(game.agents.get(temp).path)
     MyClient.stop_connection()
     exit(0)
+
 
 if __name__ == "__main__":
     main()
